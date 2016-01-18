d001614f9d57c7ae1ef3aea9b8a21917a94d3204
David Daverio <daverio@pi213a.itp.uni-heidelberg.de>
2016-01-18 11:08:29 +0100

    exercice 1 heidelberg

---
+++ examples/exercice/parallel_ex1.cpp
@@ -0,0 +1,37 @@
+/*! file gettingStarted.cpp
+    Created by David Daverio.
+
+    A simple example of LATfield2 usage.
+ */
+
+
+#include "LATfield2.hpp"
+using namespace LATfield2;
+
+
+int main(int argc, char **argv)
+{
+
+    //-------- Initilization of the parallel object ---------
+    int n,m;
+
+    for (int i=1 ; i < argc ; i++ ){
+		if ( argv[i][0] != '-' )
+			continue;
+		switch(argv[i][1]) {
+			case 'n':
+				n = atoi(argv[++i]);
+				break;
+			case 'm':
+				m =  atoi(argv[++i]);
+				break;
+		}
+	}
+  parallel.initialize(n,m);
+
+    COUT << "Parallel grid size: ("<<parallel.grid_size()[0]<<","<<parallel.grid_size()[1]<<"). "<<endl;
+    //-----------------------   end   ------------------------
+
+  
+
+}
