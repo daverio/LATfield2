b564a9fe8b29d0262bae01db2d79bc6cfcc728c2
daverio <david.daverio@unige.ch>
2016-02-06 13:19:25 +0100

    parallel2d issues?

--- LATfield2_parallel2d.hpp
+++ LATfield2_parallel2d.hpp
@@ -687,8 +687,8 @@ void Parallel2d::initialize(int proc_size0, int proc_size1,int IO_total_size, in
 
 Parallel2d::~Parallel2d() 
 { 
-   for(int i=0;i<grid_size_[0];i++)MPI_Comm_free(&(dim0_comm_[i]));
-  for(int i=0;i<grid_size_[1];i++)MPI_Comm_free(&(dim1_comm_[i]));
+  //  for(int i=0;i<grid_size_[0];i++)MPI_Comm_free(&(dim0_comm_[i]));
+  //  for(int i=0;i<grid_size_[1];i++)MPI_Comm_free(&(dim1_comm_[i]));
 
   int finalized;
   MPI_Finalized(&finalized);
