950beb6f42e7b29ef69fcfabfa3813adf6977afb
David Daverio <david.daverio@unige.ch>
2016-06-16 22:55:33 +0200

    memory.cpp

--- examples/memory.cpp
+++ examples/memory.cpp
@@ -4,16 +4,26 @@
     A simple example of LATfield2 usage.
  */
 
+#include <scorep/SCOREP_User.h>
 
 #include "LATfield2.hpp"
 using namespace LATfield2;
+
 
 
 int main(int argc, char **argv)
 {
 
     //-------- Initilization of the parallel object ---------
-    int n,m;
+    int n,m,latSize;
+
+    SCOREP_USER_REGION_DEFINE(init)
+    SCOREP_USER_REGION_DEFINE(loop)
+    SCOREP_USER_REGION_DEFINE(der1)
+    SCOREP_USER_REGION_DEFINE(der2)
+    SCOREP_USER_REGION_DEFINE(der3)
+    SCOREP_USER_REGION_DEFINE(der4)
+
 
     for (int i=1 ; i < argc ; i++ ){
 		if ( argv[i][0] != '-' )
@@ -25,6 +35,9 @@ int main(int argc, char **argv)
 			case 'm':
 				m =  atoi(argv[++i]);
 				break;
+        case 's':
+  				latSize =  atoi(argv[++i]);
+  				break;
 		}
 	}
 
@@ -38,44 +51,57 @@ int main(int argc, char **argv)
 
     //------------   Declaration of a Lattice   --------------
     int dim = 3;
-    int latSize[3] = {512,512,512};
     int halo = 2;
+
+  SCOREP_USER_REGION_BEGIN( init, "initialisation",
+      SCOREP_USER_REGION_TYPE_COMMON )
+
+
     Lattice lat(dim,latSize,halo);
-/*
-    LFvector<double> a(&lat);
-    LFvector<double> b(&lat);
-    LFvector<double> c(&lat);
-
-    for(int i=0;i<32;i++)
-    {
-      a[i]=i;
-      b[i]=i;
-      c[i]=2;
-      //COUT<<a[i]<<endl;;
-    }
-
-
-    c += 2.0+(a*b);
-
-    for(int i=0;i<32;i++)
-    {
-      COUT<<"a["<<i<<"]: "<<a[i]<<" , b["<<i<<"]: "<<b[i]<<" , c["<<i<<"]: "<<c[i]<<endl;;
-    }
-*/
-
     Field<double> rho(lat,1);
     Field<double> phi(lat,3);
     Field<double> q(lat,3,3,LATfield2::symmetric);
     Field<double> K(lat,3,3,LATfield2::symmetric);
+
+    SCOREP_USER_REGION_END( init )
+
     double dx=0.2;
 
     Site x(lat);
+for(int l = 0;l<5;l++)
+{
 
+  SCOREP_USER_REGION_BEGIN( loop, "forallsites",
+      SCOREP_USER_REGION_TYPE_LOOP )
     for(x.first();x.test();x.next())
     {
-        for(int i=0;i<3;i++)phi(x,i) = (rho(x+i)-rho(x-i))/(2.0*dx);
-    }
+        SCOREP_USER_REGION_BEGIN( der1, "diRho",
+            SCOREP_USER_REGION_TYPE_COMMON )
+        for(int i=0;i<3;i++)phi(x,i) =rho(x+i)+rho(x-i)/(2.0*dx);
+        SCOREP_USER_REGION_END( der1 )
 
+        SCOREP_USER_REGION_BEGIN( der2, "didiRho",
+            SCOREP_USER_REGION_TYPE_COMMON )
+        for(int i=0;i<3;i++)q(x,i,i) =(rho(x+i) + rho(x-i) - 2.0 * rho(x))/(dx*dx);
+        SCOREP_USER_REGION_END( der2 )
+
+        SCOREP_USER_REGION_BEGIN( der3, "didjRhp",
+            SCOREP_USER_REGION_TYPE_COMMON )
+        for(int i=0;i<3;i++)for(int j=i+1;j<3;j++)q(x,i,j) = (rho(x+i+j) - rho(x-i+j) - rho(x+i-j) + rho(x-i-j))/(4.0*dx*dx);
+        SCOREP_USER_REGION_END( der3 )
+
+        SCOREP_USER_REGION_BEGIN( der4, "qijKij",
+            SCOREP_USER_REGION_TYPE_COMMON )
+        for(int i=0;i<3;i++)
+        {
+          phi(x,i) = 0;
+          for(int j=0;j<3;j++)rho(x,i) += q(x,i,j)*K(x,i,j);
+        }
+        SCOREP_USER_REGION_END( der4 )
+    }
+  SCOREP_USER_REGION_END( loop )
+}
+/*
     for(x.first();x.test();x.next())
     {
         for(int i=0;i<3;i++)q(x,i,i) = (rho(x+i) + rho(x-i) - 2.0 * rho(x))/(dx*dx);
@@ -87,7 +113,7 @@ int main(int argc, char **argv)
         rho(x) = 0;
         for(int i=0;i<3;i++)for(int j=0;j<3;j++)rho(x) += q(x,i,j)*K(x,i,j);
     }
-
+*/
 
 /*
     for(int i=0;i<32;i++)
