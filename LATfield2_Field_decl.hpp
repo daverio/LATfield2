810fdbb84a660eb50c23cd5a8e34c88ee3fa99fb
David Daverio <david.daverio@unige.ch>
2017-10-30 19:22:26 +0100

    adding getCompSym to Field_decl.hpp

--- LATfield2_Field_decl.hpp
+++ LATfield2_Field_decl.hpp
@@ -9,6 +9,14 @@
 
 //Matrix-style component symmetry
 
+int getCompSym(int i,int j)
+{
+  int component;
+  if (i>j) component = i + j * 3 - (j * (1 + j)) / 2;
+  else component = j + i * 3 - (i * (1 + i)) / 2;
+  return component;
+}
+
 extern  int symmetric;
 extern  int unsymmetric;
 
